a1b9b62c9787fd9e75c8b008828812fadcdbfb75
RaulinSS <rsoto_1989@hotmail.com>
2017-05-15 23:47:38 -0300

    se implementou o server.js para utilizar o webpackMiddleware.

--- src/index.js
+++ src/index.js
@@ -2,12 +2,3 @@ import css from './delete-after-app.scss';
 
 console.log("hola que tal");
 
-/*
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-ReactDOM.render(
-    <p>Hello, World</p>,
-    document.getElementById("root")
-);*/
-
--- webpack/config-dev.js
+++ webpack/config-dev.js
@@ -8,44 +8,19 @@ const defaults = require('./defaults');
 module.exports = Object.assign(defaults, {
     entry: [
         // activate HMR for React
-            //'react-hot-loader/patch',
-
-        // bundle the client for webpack-dev-server
-        // and connect to the provided endpoint
-        //'webpack-dev-server/client?http://localhost:8080',
-
-        // bundle the client for hot reloading
-        // only- means to only hot reload for successful updates
-        //'webpack/hot/only-dev-server',
-
+        'react-hot-loader/patch',
+        'webpack-hot-middleware/client?http://localhost:3000/',
         // the entry point of our app
-        //'./index.js'
         `${PATHS.src}/index.js`
     ],
-
-    devServer: {
-        // enable HMR on the server
-        hot: true,
-
-        // match the output path
-        contentBase: join(__dirname, 'dist'),
-
-        //configure the port
-            //port: 9000,
-        
-        // match the output `publicPath`
-            //publicPath: '/'
-
-        //option for open the window automatically
-        open: true
-    },
-
     plugins: [
         // enable HMR globally
         new webpack.HotModuleReplacementPlugin(),
     
         // prints more readable module names in the browser console on HMR updates
         new webpack.NamedModulesPlugin(),
+
+        new webpack.NoEmitOnErrorsPlugin(),
 
         //create virtual index.html or custom index template
         new HtmlWebpackPlugin({
